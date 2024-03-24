a3502ab127270bf94cf0056ed608685810c642e6
Kashish Singh <117498422+kashishsinghyadav@users.noreply.github.com>
2024-03-24 21:37:35 +0530

    Update singletonpattern.py

--- CreationalPatterns/singletonpattern.py
+++ CreationalPatterns/singletonpattern.py
@@ -1,1 +1,17 @@
 
+class DatabaseConnection:
+    _instance = None
+    
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._instance.connect()
+        return cls._instance
+    
+    def connect(self):
+        print("Connecting to the database...")
+
+connection1 = DatabaseConnection()
+connection2 = DatabaseConnection()
+
+print(connection1 is connection2)  
